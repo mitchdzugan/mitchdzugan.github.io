4ccf61e6c24da2102f3e12866fd1765133f27b34
mitchdzugan <mitchdzugan@gmail.com>
2018-12-17 14:13:50 -0800

    inital commit

---
+++ precache-manifest.c21741403de7a6073d66d597a2dd20f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a9dd9661347d355366f",
+    "url": "/static/js/main.5a9dd966.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "5a9dd9661347d355366f",
+    "url": "/static/css/main.e3229641.chunk.css"
+  },
+  {
+    "revision": "b064d68fe017f0067b98ab19952e7043",
+    "url": "/index.html"
+  }
+];
